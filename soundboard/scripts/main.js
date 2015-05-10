94d48805471ccdd1a380a81c19202a21c73e458b
Nick Stoneman <nick.stoneman@gmail.com>
2015-05-10 23:10:29 +0100

    fixed audio added css styling

---
+++ soundboard/scripts/main.js
@@ -0,0 +1,13 @@
+// soundManager.setup({
+//   url: '/path/to/swf-files/',
+//   onready: function() {
+//     var mySound = soundManager.createSound({
+//       id: 'aSound',
+//       url: '/Users/Nick/Desktop/402_ Ruth Reichl on Challenging Career Moves.mp3'
+//     });
+//     mySound.play();
+//   },
+//   ontimeout: function() {
+//     // Hrmm, SM2 could not start. Missing SWF? Flash blocked? Show an error, etc.?
+//   }
+// });
