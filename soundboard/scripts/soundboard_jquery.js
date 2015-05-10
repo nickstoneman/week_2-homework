4ce04c19703b51f8573c27418812d8a615162da5
Nick Stoneman <nick.stoneman@gmail.com>
2015-05-10 19:01:44 +0100

    got sound working and menu list working

---
+++ soundboard/scripts/soundboard_jquery.js
@@ -0,0 +1,20 @@
+var menutItem = [];
+var adjectives = ['tasty', 'delicious', 'special', 'extremely rare', 'endangered'];
+
+$(document).ready(function() {
+// MAIN JS IN jQuery !!!!
+console.log('jQuery works!');
+
+  $('.greeting').on("click", function() {
+    console.log("click");
+    var output = $(this).attr('id'); 
+    var randomAdjective = adjectives[Math.floor(adjectives.length * Math.random())];
+    $('ol').append('<li>' + output + randomAdjective + '</li>');
+    
+  });
+
+});
+
+
+
+
