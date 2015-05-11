9ea2883e3da76ef928e4a06014c7a0d15073803e
Nick Stoneman <nick.stoneman@gmail.com>
2015-05-11 11:25:39 +0100

    replaced podcasts with sounds

--- soundboard/scripts/main.js
+++ soundboard/scripts/main.js
@@ -1,12 +1,12 @@
-// soundManager.setup({
-//   url: '/path/to/swf-files/',
-//   onready: function() {
-//     var mySound = soundManager.createSound({
-//       id: 'aSound',
-//       url: '/Users/Nick/Desktop/402_ Ruth Reichl on Challenging Career Moves.mp3'
-//     });
-//     mySound.play();
-//   },
+soundManager.setup({
+  url: 'audiofiles/',
+  onready: function() {
+    var mySound = soundManager.createSound({
+      id: 'aSound',
+      url: '/audiofiles'
+    });
+    mySound.play();
+  },
 //   ontimeout: function() {
 //     // Hrmm, SM2 could not start. Missing SWF? Flash blocked? Show an error, etc.?
 //   }
--- soundboard/scripts/soundboard_jquery.js
+++ soundboard/scripts/soundboard_jquery.js
@@ -1,17 +1,45 @@
 var menutItem = [];
-var adjectives = ['tasty', 'delicious', 'special', 'extremely rare', 'endangered'];
+var adjectives = ['tasty', 'delicious', 'special', 'extremely rare', 'endangered', 'very tasty', 'house special'];
 
 $(document).ready(function() {
 // MAIN JS IN jQuery !!!!
 console.log('jQuery works!');
 
+
+
+
   $('.greeting').on("click", function() {
     console.log("click");
     var output = $(this).attr('id'); 
     var randomAdjective = adjectives[Math.floor(adjectives.length * Math.random())];
-    $('ol').append('<li>' + output + randomAdjective + '</li>');
+    $('ol').append('<li>' + randomAdjective + " " + output + '</li>');
     
   });
+
+// Sounds
+$('#ebi').on("click", function(){
+    var sushiEbi = soundmanager.createSound({
+        url: "audiofiles/two_shrimp_sushi_-_yes.mp3"
+        });
+    sushiEbi.play();
+    })
+
+$('.tamago').on("click", function(){
+    var sushiTamago = soundmanager.createSound({
+        url: "path to file.mp3"
+        });
+    sushiTamago.play();
+    })
+
+$('.gunkan_maki').on("click", function(){
+    var gunkan_maki = soundmanager.createSound({
+        url: "path to file.mp3"
+        });
+    sushiGunkan_maki.play();
+    })
+
+
+
 
 });
 
